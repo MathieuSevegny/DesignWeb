987e2b23a0814619c0a5cc5aa4bc7a1b932d7a8c
MathieuSevegny <sevegny.mathieu@carrefour.cegepvicto.ca>
2019-11-22 16:06:58 -0500

    Optimisation du JS

--- DesignWeb/Photociti/js/image.js
+++ DesignWeb/Photociti/js/image.js
@@ -48,9 +48,11 @@ $('#word').keyup(function () {
         return this.value;
     }
 });
+
 $('#go').click(function () {
     go()
 });
+
 $('#word').keypress(function (event) {
     var keycode = (event.keyCode ? event.keyCode : event.which);
     if (keycode == '13') {
@@ -63,75 +65,35 @@ function go() {
     if (text.length >= 3 && text.length <= 12) {
         if (text.length === 3) {
             $('.lettres').attr("class", "col-4 lettres p-0 m-0");
-            $('#4').hide();
-            $('#5').hide();
-            $('#6').hide();
-            $('#7').hide();
-            $('#8').hide();
-            $('#9').hide();
-            $('#10').hide();
-            $('#11').hide();
-            $('#12').hide();
+            for (var i = 4; i <= 12; i++) {
+                $('#' + i).hide();
+            }
         } else if (text.length === 4) {
             $('.lettres').attr("class", "col-3 lettres p-0 m-0");
             $('#4').show();
-            $('#5').hide();
-            $('#6').hide();
-            $('#7').hide();
-            $('#8').hide();
-            $('#9').hide();
-            $('#10').hide();
-            $('#11').hide();
-            $('#12').hide();
+            for (i = 5; i <= 12; i++) {
+                $('#' + i).hide();
+            }
         } else if (text.length === 5 || text.length === 6) {
             $('.lettres').attr("class", "col-2 lettres p-0 m-0");
             $('#4').show();
             $('#5').show();
             $('#6').show();
-            $('#7').hide();
-            $('#8').hide();
-            $('#9').hide();
-            $('#10').hide();
-            $('#11').hide();
-            $('#12').hide();
+            for (i = 7; i <= 12; i++) {
+                $('#' + i).hide();
+            }
         } else if (text.length >= 7) {
             $('.lettres').attr("class", "col-1 lettres p-0 m-0");
-            $('#4').show();
-            $('#5').show();
-            $('#6').show();
-            $('#7').show();
-            $('#8').show();
-            $('#9').show();
-            $('#10').show();
-            $('#11').show();
-            $('#12').show();
+            $('.couleur').show();
         }
         letterplacement()
     }
 }
 
-$('#blanc').click(function () {
-    $('#background').css("background-image", 'url("background/blanc.jpg")');
+$('.couleur').click(function () {
+    var couleur = $(this).attr("id");
+    $('#background').css("background-image", 'url("background/'+ couleur +'.jpg")');
 });
-$('#bleu').click(function () {
-    $('#background').css("background-image", 'url("background/bleu.jpg")');
-});
-$('#bleuFonce').click(function () {
-    $('#background').css("background-image", 'url("background/bleuFonce.jpg")');
-});
-$('#noir').click(function () {
-    $('#background').css("background-image", 'url("background/noir.jpg")');
-});
-$('#gris').click(function () {
-    $('#background').css("background-image", 'url("background/gris.jpg")');
-});
-$('#rouge').click(function () {
-    $('#background').css("background-image", 'url("background/rouge.jpg")');
-});
-$('#rose').click(function () {
-    $('#background').css("background-image", 'url("background/rose.jpg")');
-});
-
 
 $('.lettre').click(function () {
     nb = $(this).attr('id');
@@ -140,11 +102,9 @@ $('.lettre').click(function () {
     } else {
         car = mot[nb - 1].toUpperCase();
     }
-    $('#car1').attr("src", "Letters/" + car + "/" + car + "1.jpg");
-    $('#car2').attr("src", "Letters/" + car + "/" + car + "2.jpg");
-    $('#car3').attr("src", "Letters/" + car + "/" + car + "3.jpg");
-    $('#car4').attr("src", "Letters/" + car + "/" + car + "4.jpg");
-    $('#car5').attr("src", "Letters/" + car + "/" + car + "5.jpg");
+    for (var i = 1; i<=5; i++) {
+        $('#car'+i).attr("src", "Letters/" + car + "/" + car + i + ".jpg");
+    }
     $('#choix2').modal('show');
 });
 
@@ -158,11 +118,4 @@ $('#save').click(function () {
     $("#" + nb).attr('src', "Letters/" + car + "/" + car + nbcar + ".jpg");
     $('#myCarousel').carousel(0);
     $('#choix2').modal('hide');
-});
-
-$('#word').onkeyup(function () {
-    var str = this.value;
-    alert(str + "..");
-
-    return input;
 });
